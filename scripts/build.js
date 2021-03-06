3ffee37a6a78019b0c92d2cdb157cc79951be224
刘银 <liu334275@gmail.com>
2021-03-06 12:54:17 +0800

    build: 添加构建进度条与语法检查

--- scripts/build.js
+++ scripts/build.js
@@ -4,11 +4,13 @@
  * @Author: liuyin
  * @Date: 2021-03-04 00:45:15
  * @LastEditors: liuyin
- * @LastEditTime: 2021-03-04 23:02:27
+ * @LastEditTime: 2021-03-06 12:52:18
  */
 const fs = require('fs');
 const path = require('path');
 const babel = require('@babel/core');
+const proc = require('child_process');
+const ProgressBar = require('progress');
 const { components, removeDir } = require('./utils');
 
 // 组件库目录
@@ -20,28 +22,44 @@ const scriptRegx = /\.tsx?$/;
 // 输出目录
 const outDir = './es';
 
-// 获取所有组件
-const src = components(PATH, scriptRegx);
-
 // 清理上一次构建结果
 removeDir(outDir);
 removeDir('./types');
 
-if (!fs.existsSync(outDir)) {
-  fs.mkdirSync(outDir);
-}
+try {
+  // 获取所有组件
+  const src = components(PATH, scriptRegx);
 
-// 编译文件
-src.forEach((v) => {
-  const { code } = babel.transformFileSync(v);
-  const filename = v.replace(PATH, outDir).replace(scriptRegx, '.js');
-  const p = path.dirname(filename);
-  // 创建文件夹
-  if (!fs.existsSync(p)) {
-    fs.mkdirSync(p);
+  // 编译 ts，生成 `.d.ts`
+  proc.execSync(`tsc`, { stdio: 'inherit' });
+
+  const bar = new ProgressBar(':current/:total [:bar] :percent', {
+    total: src.length,
+    width: 30,
+    complete: '=',
+    clear: false,
+    callback() {
+      process.stderr.write('Compile success!\n\n');
+    },
+  });
+
+  if (!fs.existsSync(outDir)) {
+    console.log('Create es directory\n');
+    fs.mkdirSync(outDir);
   }
-  fs.writeFileSync(filename, code);
-});
 
-// 编译 ts，生成 `.d.ts`
-require('typescript/lib/tsc');
+  // 编译文件
+  src.forEach((v) => {
+    const { code } = babel.transformFileSync(v);
+    const filename = v.replace(PATH, outDir).replace(scriptRegx, '.js');
+    const p = path.dirname(filename);
+    // 创建文件夹
+    if (!fs.existsSync(p)) {
+      fs.mkdirSync(p);
+    }
+    fs.writeFileSync(filename, code);
+    bar.tick(1);
+  });
+} catch (e) {
+  console.error(e);
+}
